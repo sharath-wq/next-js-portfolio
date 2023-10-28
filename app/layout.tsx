cad3dc242daeba8e37a02aba07144720b804339a
sharath-wq <chandranpsharath@gmail.com>
2023-10-28 20:21:16 +0530

    created the component basic structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,24 @@
+import { Header } from "@/components";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Sharath | Personal Portfolio",
-  description:
-    "Self-taught Full-Stack Developer | Web Development Enthusiast | Crafting digital solutions with passion and expertise. Explore my journey in the world of web development.",
+    title: "Sharath | Personal Portfolio",
+    description:
+        "Self-taught Full-Stack Developer | Web Development Enthusiast | Crafting digital solutions with passion and expertise. Explore my journey in the world of web development.",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body className={`${inter.className} bg-gray-50 text-gray-900 relative`}>
-        <div className="bg-[#b7d9b8] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#6e946e]"></div>
-        <div className="bg-[#a7b6f5] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#606e94]"></div>
-
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={`${inter.className} bg-gray-50 text-gray-900 relative h-[5000px]`}>
+                <div className="bg-[#b7d9b8] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#6e946e]"></div>
+                <div className="bg-[#a7b6f5] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#606e94]"></div>
+                <Header />
+                {children}
+            </body>
+        </html>
+    );
 }
