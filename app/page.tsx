696ab03e15b7485fcde263ad3d86923fc855a17c
sharath-wq <chandranpsharath@gmail.com>
2023-10-29 12:27:00 +0530

    divider completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import { Intro } from "@/components";
+import { Intro, SectionDivider } from "@/components";
 
 export default function Home() {
     return (
         <main className="flex flex-col items-center px-4">
             <Intro />
+            <SectionDivider />
         </main>
     );
 }
