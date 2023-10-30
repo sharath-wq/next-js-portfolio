8728137bb0a0acb08bfe5909bebae3136e35e75d
sharath-wq <chandranpsharath@gmail.com>
2023-10-30 23:48:08 +0530

    fixed the contact me button animation issues

--- components/intro.tsx
+++ components/intro.tsx
@@ -8,8 +8,10 @@ import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
+import { useActiveSectionContext } from "@/context/active-section-context";
 
 const Intro = () => {
+    const { activeSection, setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
     const { ref } = useSectionInView("Home", 0.5);
     return (
         <section ref={ref} id="home" className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]">
@@ -68,8 +70,12 @@ const Intro = () => {
                 }}
             >
                 <Link
-                    href="#contacts"
+                    href="#contact"
                     className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+                    onClick={() => {
+                        setActiveSection("Contact");
+                        setTimeOfLastClick(Date.now());
+                    }}
                 >
                     Contact me here <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
                 </Link>
