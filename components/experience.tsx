1785e85a72227b258cbbfee337ba731d6c0025cf
sharath-wq <chandranpsharath@gmail.com>
2023-10-30 21:49:06 +0530

    fixed the experience animation issue

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,22 +1,22 @@
 "use client";
 
-import { useSectionInView } from "@/lib/hooks";
 import React from "react";
 import SectionHeading from "./section-heading";
 import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import { experiencesData } from "@/lib/data";
+import { useSectionInView } from "@/lib/hooks";
 
 const Experience = () => {
     const { ref } = useSectionInView("Experience");
+
     return (
         <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
             <SectionHeading text="my experience" />
-            <VerticalTimeline lineColor="" animate={true}>
+            <VerticalTimeline lineColor="">
                 {experiencesData.map((item, index) => (
                     <React.Fragment key={index}>
                         <VerticalTimelineElement
-                            visible={true}
                             contentStyle={{
                                 background: "#f3f4f6",
                                 boxShadow: "none",
