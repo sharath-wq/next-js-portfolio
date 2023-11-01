f9a4a4f630a5a290a41ca2bb6027a881a0fbb3b9
sharath-wq <chandranpsharath@gmail.com>
2023-11-01 15:56:18 +0530

    dark mode header completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,11 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en" className="!scroll-smooth">
-            <body className={`${inter.className} bg-gray-50 text-gray-900 relative pt-28 sm:pt-36`}>
-                <div className="bg-[#ADD8E6] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#FFB6C1]"></div>
-                <div className="bg-[#FFB6C1] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#ADD8E6]"></div>
+            <body
+                className={`${inter.className} bg-gray-50 text-gray-900 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
+            >
+                <div className="bg-[#ADD8E6] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
+                <div className="bg-[#FFB6C1] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
 
                 <ActiveSectionContextProvider>
                     <Header />
--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -11,9 +11,11 @@ const ThemeSwitch = () => {
         if (theme === "light") {
             setTheme("dark");
             window.localStorage.setItem("theme", "dark");
+            document.documentElement.classList.add("dark");
         } else {
             setTheme("light");
             window.localStorage.setItem("theme", "light");
+            document.documentElement.classList.remove("dark");
         }
     };
 
@@ -22,14 +24,20 @@ const ThemeSwitch = () => {
 
         if (localTheme) {
             setTheme(localTheme);
+            if (localTheme === "dark") {
+                document.documentElement.classList.add("dark");
+            } else {
+                document.documentElement.classList.remove("dark");
+            }
         } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
             setTheme("dark");
+            document.documentElement.classList.add("dark");
         }
     }, []);
 
     return (
         <button
-            className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all"
+            className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-black border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950 "
             onClick={toggleTheme}
         >
             {theme === "light" ? <BsSun /> : <BsMoon />}
