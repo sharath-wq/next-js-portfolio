81da8f9afefbe693c14925e817b9499528b9284e
Sharath-wq <chandranpsharath@gmail.com>
2024-05-21 20:01:35 +0530

    resume updated

--- components/intro.tsx
+++ components/intro.tsx
@@ -81,7 +81,7 @@ const Intro = () => {
                     Contact me here <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition' />
                 </Link>
                 <a
-                    href='https://drive.google.com/file/d/17pVuH4QjIP8lPZpXI2gGEj8g2OnhyApq/view?usp=share_link'
+                    href='https://drive.google.com/file/d/1j3GTqS6FdvAp3YXI-2JADlsip42dyKCm/view?usp=share_link'
                     download
                     className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110  active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10'
                 >
